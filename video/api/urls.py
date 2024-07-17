acd4d1d232cf85a3f1c50a78cf8b8b4d5b5c28f2
adrianapps <bulsar@tutanota.com>
2024-07-17 23:51:42 +0200

    add WatchLater endpoints

--- video/api/urls.py
+++ video/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
-from .views import VideoList, VideoDetail, TagList, TagDetail, CommentList, CommentDetail
+from .views import (
+    VideoList, VideoDetail, TagList, TagDetail, CommentList, CommentDetail, WatchLaterList, WatchLaterDetail
+)
 
 urlpatterns = [
     path('channels/<int:channel_id>/videos/', VideoList.as_view(), name='channel-videos'),
@@ -9,5 +11,7 @@ urlpatterns = [
     path('videos/<int:video_id>/comments/', CommentList.as_view(), name='video-comments'),
     path('videos/<int:video_id>/comments/<int:comment_id>/', CommentDetail.as_view(), name='comment-detail'),
     path('tags/', TagList.as_view(), name='tag-list'),
-    path('tags/<int:tag_id>', TagDetail.as_view(), name='tag-detail'),
+    path('tags/<int:tag_id>/', TagDetail.as_view(), name='tag-detail'),
+    path('users/<int:user_id>/watch_later/', WatchLaterList.as_view(), name='watch-later-list'),
+    path('users/<int:user_id>/watch_later/<int:watch_later_id>', WatchLaterDetail.as_view(), name='watch-later-detail'),
 ]
