bd8096ef49a79264768ed35000b08fefeb5047b6
adrianapps <bulsar@tutanota.com>
2024-07-24 18:39:05 +0200

    add subscribers and avatar to ChannelFactory

--- channel/api/tests/factories.py
+++ channel/api/tests/factories.py
@@ -1,21 +1,36 @@
 import factory
-from factory.django import DjangoModelFactory
+from factory.django import DjangoModelFactory, ImageField
 from channel.models import User, Channel
 
 
 class UserFactory(DjangoModelFactory):
+    class Meta:
+        model = User
+
     username = factory.Faker("user_name")
     email = factory.Faker("email")
     password = factory.Faker("password")
 
-    class Meta:
-        model = User
-
 
 class ChannelFactory(DjangoModelFactory):
+    class Meta:
+        model = Channel
+        exclude = ('files',)
+
     name = factory.Faker("word")
     user = factory.SubFactory(UserFactory)
     description = factory.Faker("text")
+    avatar = ImageField()
+    files = set()
 
-    class Meta:
-        model = Channel
+    @factory.post_generation
+    def post(self, created, extracted, **kwargs):
+        ChannelFactory.files.add(self.avatar)
+
+    @factory.post_generation
+    def subscribers(self, created, extracted, **kwargs):
+        if not created:
+            return
+        if extracted:
+            for subscriber in extracted:
+                self.subscribers.add(subscriber)
