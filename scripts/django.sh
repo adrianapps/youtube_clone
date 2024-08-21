0f71f06750b3906d40c112f8f8dc069fe45a0bcd
adrianapps <bulsar@tutanota.com>
2024-08-21 18:18:46 +0200

    skip collecting static files when running tests

--- scripts/django.sh
+++ scripts/django.sh
@@ -2,14 +2,23 @@
 echo "Migrate"
 python manage.py migrate --no-input
 echo "=================================="
-echo "Collect static files"
-python manage.py collectstatic --no-input
-echo "=================================="
 echo "Prepopulate S3 bucket"
 python /app/scripts/prepopulate_s3.py
 
-if [ $# -eq 0 ]; then
+if [[ $* == *"test"* ]]; then
   echo "=================================="
-  echo "Start server"
-  exec python manage.py runserver 0.0.0.0:8000
+  echo "Run tests"
+  exec "$@"
+else
+  echo "=================================="
+  echo "Collect static files"
+  python manage.py collectstatic --no-input
+  
+  if [ $# -eq 0 ]; then
+    echo "=================================="
+    echo "Start server"
+    exec python manage.py runserver 0.0.0.0:8000
+  else
+    exec "$@"
+  fi
 fi
