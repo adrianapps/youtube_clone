16d50de5e5b221af688be90addb66339aecad50a
adrianapps <bulsar@tutanota.com>
2024-08-26 19:04:50 +0200

    fix conditional

--- video/validators.py
+++ video/validators.py
@@ -5,5 +5,5 @@ from django.core.exceptions import ValidationError
 def validate_file_extension(value):
     ext = os.path.splitext(value.name)[1]
     allowed_extensions = ['.mp4', '.avi', '.mkv', '.webm', '.mov', '.wmv']
-    if not ext.lower() in allowed_extensions:
+    if ext.lower() not in allowed_extensions:
         raise ValidationError('Unsupported file extension.')
